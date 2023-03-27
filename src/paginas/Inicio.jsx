f940e776172ec5c967df68ddbdb62d8a70cb331d
ZitelliDZ <zitelli.dz@gmail.com>
2023-03-27 08:16:17 -0300

    Segundo commit

--- src/paginas/Inicio.jsx
+++ src/paginas/Inicio.jsx
@@ -10,7 +10,7 @@ const Inicio = () => {
 
         const obtenerClientesApi = async () => {
             try {
-                const url = "http://localhost:4000/clientes";
+                const url = import.meta.env.VITE_API_URL;
                 const respuesta = await fetch(url);
                 const resultado = await respuesta.json();
                 setClientes(resultado);
@@ -27,7 +27,7 @@ const Inicio = () => {
             const confirmar = confirm(`Desea Eliminar el cliente?${id}`)
 
             if (confirmar) {
-                const url = `http://localhost:4000/clientes/${id}`;
+                const url = `${import.meta.env.VITE_API_URL}/${id}`;
                 const respuesta = await fetch(url, {
                     method: 'DELETE',
                 })
